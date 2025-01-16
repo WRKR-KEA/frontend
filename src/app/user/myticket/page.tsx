0c4fb234934045feb3981a6de8748effbce74e2a
yeonge2 <sall3497@gachon.ac.kr>
2025-01-16 23:32:03 +0900

    WRKR-16:✨:Feat:티켓조회 상태 필터링 기능 추가

--- src/app/user/myticket/page.tsx
+++ src/app/user/myticket/page.tsx
@@ -1,7 +1,7 @@
 "use client"; // 클라이언트 컴포넌트로 지정
 
 import { useState } from "react";
-import { TicketList } from "../../../components/ticketList";
+import { TicketList_Filter } from "../../../components/ticketList_Filter";
 import { FilterNum } from "../../../components/filterNum"; 
 import PagePagination from "../../../components/pagination"; 
 
@@ -24,7 +24,7 @@ export default function UserTicketListPage() {
         <h2 className="text-md font-semibold">티켓 조회</h2>
         <FilterNum onSelectCount={handleSelectCount} />
       </div>
-      <TicketList maxTicketsToShow={maxTicketsToShow} page={currentPage} />
+      <TicketList_Filter maxTicketsToShow={maxTicketsToShow} page={currentPage} />
       
       {/* 페이지네이션 컨테이너 */}
       <div className="flex justify-center items-center mt-4">

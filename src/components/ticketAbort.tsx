0b348d753fa8b3e74fb399c8f892a6c5a285c5a0
yeonge2 <sall3497@gachon.ac.kr>
2025-01-22 09:21:59 +0900

    WRKR-59:✨:Feat:티켓 반려 사유 작성 부분 주석처리

--- src/components/ticketAbort.tsx
+++ src/components/ticketAbort.tsx
@@ -9,10 +9,10 @@ export function TicketAbort({ isOpen, onClose, onConfirm }) {
 
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-gray-500 bg-opacity-50 z-50">
-      <div className="bg-white w-[600px] p-6 rounded-lg text-center shadow-lg">
+      <div className="bg-white p-6 rounded-lg text-center shadow-lg">
         <p className="text-black text-lg font-medium">티켓 작업을 반려하시겠습니까?</p>
         <p className="text-red-500 text-sm mb-8">반려 후 복구할 수 없어요.</p>
-        <div className="mb-4">
+        {/* <div className="mb-4">
           <label
             htmlFor="content"
             className="block text-sm font-medium mb-1 ml-1 text-left" 
@@ -27,7 +27,7 @@ export function TicketAbort({ isOpen, onClose, onConfirm }) {
             value={content}
             onChange={(e) => setContent(e.target.value)}
           />
-        </div>
+        </div> */}
         <div className="flex justify-center space-x-4">
           <Button label="취소" onClick={onClose} color={4} />
           <Button label="반려하기" onClick={() => onConfirm(content)} color={2} />

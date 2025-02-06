6f463cf3271452c80661aed964a9cf7042b5df97
strongmhk <kim6562166086@gmail.com>
2025-02-06 18:38:47 +0900

    WRKR-151 ♻️ Refactor: api 호출시 header에 토큰 추가
    <footer>
    #51

--- src/lib/api/axios.ts
+++ src/lib/api/axios.ts
@@ -1,15 +1,10 @@
 import axios, { AxiosInstance } from "axios";
 
-const BASE_URL = "http://172.16.211.53:8080";
-const accessToken =
-  "eyJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoiVVNFUiIsInR5cGUiOiJBQ0NFU1MiLCJzdWIiOiJ1c2VyLmN5dyIsImV4cCI6MTc0MDMxMDg0N30.YdTnsOwMVORmdhwmw6pM5IC1CWK-2BVRvyPEgfjQdOI";
+const accessToken = sessionStorage.getItem("accessToken");
 
-// 최상위에 .env 파일을 만들고
-// NEXT_PUBLIC_BASE_URL='http://172.16.211.53:8080/' 를 추가한 다음 아래의 주석을 풀어주세요
 const apiConfig = {
   backend: {
-    // baseURL: process.env.NEXT_PUBLIC_BASE_URL,
-    baseURL: BASE_URL,
+    baseURL: process.env.NEXT_PUBLIC_BASE_URL,
   },
 };
 
@@ -20,6 +15,7 @@ const api: AxiosInstance = axios.create({
   withCredentials: true, // 쿠키를 포함한 인증 정보를 서버에 전송
   headers: {
     "Content-Type": "application/json",
+    "Authorization": `Bearer ${accessToken}`,
   },
 });
 

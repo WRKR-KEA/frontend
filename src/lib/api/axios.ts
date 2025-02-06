a23a782cd3b20663a47181f920c65f17cc4b682c
qlfyd123 <86708481+qlfyd123@users.noreply.github.com>
2025-02-06 20:49:46 +0900

    WRKR-149 Feature: 월간 통계 데이터 연동

--- src/lib/api/axios.ts
+++ src/lib/api/axios.ts
@@ -1,8 +1,9 @@
-import axios, { AxiosInstance } from "axios";
+'use client';
 
-const BASE_URL = "http://172.16.211.53:8080";
-const accessToken =
-  "eyJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoiVVNFUiIsInR5cGUiOiJBQ0NFU1MiLCJzdWIiOiJ1c2VyLmN5dyIsImV4cCI6MTc0MDMxMDg0N30.YdTnsOwMVORmdhwmw6pM5IC1CWK-2BVRvyPEgfjQdOI";
+import axios, { AxiosInstance } from 'axios';
+
+const BASE_URL = 'http://172.16.211.53:8080';
+const accessToken = sessionStorage.getItem('accessToken');
 
 // 최상위에 .env 파일을 만들고
 // NEXT_PUBLIC_BASE_URL='http://172.16.211.53:8080/' 를 추가한 다음 아래의 주석을 풀어주세요
@@ -19,7 +20,8 @@ const api: AxiosInstance = axios.create({
   baseURL: server,
   withCredentials: true, // 쿠키를 포함한 인증 정보를 서버에 전송
   headers: {
-    "Content-Type": "application/json",
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${accessToken}`,
   },
 });
 

fdc6fc3375100404742705be3645ec02de0b242c
yeonge2 <sall3497@gachon.ac.kr>
2025-02-07 15:57:56 +0900

    충돌 해결

--- src/components/Modals/Help.tsx
+++ src/components/Modals/Help.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 
 interface HelpProps {
+  title: string;
   content: string;
 }
 
-const Help: React.FC<HelpProps> = ({ content }) => {
+const Help: React.FC<HelpProps> = ({ title: title, content: content }) => {
   return (
     <div>
-      <h2 className="text-lg font-bold">{content} 도움말</h2>
+      <h2 className="text-lg font-bold">{title} 도움말</h2>
       <p className="text-sm mt-2">{content}</p>
     </div>
   );

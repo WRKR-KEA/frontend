5ddd6a4123f6fc4fc8be880e714d43ac9465e434
Sujin Kang <1214sujin@naver.com>
2025-02-14 15:00:41 +0900

    WRKR-336 :bug: Fix: 카테고리 약어 형식 및 input 상태에 따른 button disable 처리

--- src/components/Modals/InputModal.tsx
+++ src/components/Modals/InputModal.tsx
@@ -21,43 +21,57 @@ export default function InputModal({
     setCategoryDescription,
     handleAddCategory
 }: InputModalProps) {
-    const [inputValue, setInputValue] = useState("");
-    const [inputValue2, setInputValue2] = useState(""); // 두 번째 인풋 필드 상태 추가
+    const [inputAbb, setInputAbb] = useState("");
+    const [inputName, setInputName] = useState(""); // 두 번째 인풋 필드 상태 추가
+  const [isValidAbb, setIsValidAbb] = useState(true);
 
-    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const handleInputAbb = (event: React.ChangeEvent<HTMLInputElement>) => {
         const newValue = event.target.value;
-        setInputValue(newValue)
-        setCategoryAbb(newValue)
+      const abbRegex = /^[A-Z]{2}/;
 
+      setIsValidAbb(!newValue || abbRegex.test(newValue));
+
+        setInputAbb(newValue)
+        setCategoryAbb(newValue)
     };
 
-    const handleInputChange2 = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const handleInputName = (event: React.ChangeEvent<HTMLInputElement>) => {
         const newValue = event.target.value;
-        setInputValue2(newValue); // 즉시 업데이트
+        setInputName(newValue); // 즉시 업데이트
         setCategoryName(newValue)
         setCategoryDescription && setCategoryDescription(newValue);
     };
+
+  const [isDisabled, setIsDisabled] = useState(true);
+  useEffect(() => {
+    setIsDisabled(!(inputName && inputAbb && isValidAbb))
+  }, [inputAbb, inputName]);
 
     return (
         <div className="flex flex-col items-center gap-3.5 pb-4 w-full">
             <h2 className="text-lg font-bold pt-6">{title}</h2>
 
             {/* 첫 번째 인풋 */}
-            <input
+            <div className="w-full max-w-md">
+              <input
                 type="text"
-                placeholder="약어를 입력하세요. ex)VM"
-                value={inputValue}
-                onChange={handleInputChange}
-                className="w-full max-w-md px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-grey-700 focus:border-blue-800"
-            />
+                placeholder="약어를 입력하세요. ex) VM"
+                value={inputAbb}
+                onChange={handleInputAbb}
+                className="w-full max-w-md px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-2 focus:border-main-1"
+              />
+              {isValidAbb || (
+                <p className="text-red-500 text-sm mt-2">카테고리 약어는 영문 대문자로 2자여야 합니다.</p>
+              )}
+            </div>
 
             {/* 두 번째 인풋 */}
             <input
                 type="text"
                 placeholder="카테고리 이름을 입력하세요."
-                value={inputValue2}
-                onChange={handleInputChange2}
-                className="w-full max-w-md px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-grey-700 focus:border-blue-800"
+                value={inputName}
+                onChange={handleInputName}
+                className="w-full max-w-md px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-2 focus:border-main-1"
             />
 
             {/* 버튼을 가로로 길게 변경 */}
@@ -65,14 +79,14 @@ export default function InputModal({
                 onClick={() => {
                     handleAddCategory();
                     onClick();
-                    setInputValue("")
-                    setInputValue2("")
+                    setInputAbb("")
+                    setInputName("")
                     setCategoryAbb("")
                     setCategoryName("")
                 }}
-                className={`px-8 py-2 text-white rounded-md text-center transition-all duration-200 
-            ${inputValue.trim() === "" ? "bg-gray-400 cursor-not-allowed" : "bg-gray-600 hover:bg-gray-700"}`}
-                disabled={inputValue.trim() === ""}
+                className="px-8 py-2 text-white bg-main-1 rounded-md text-center transition-all duration-200
+                hover:bg-main-hover disabled:opacity-50 disabled:hover:bg-main-1"
+                disabled={isDisabled}
             >
                 {btnText || "확인"}
             </button>

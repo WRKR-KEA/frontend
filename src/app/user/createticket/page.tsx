496ca77dbfee0fa917e1e938d1202f7054709dba
jiwon <kiwijomn@gmail.com>
2025-02-17 19:58:48 +0900

    WRKR-388 🐛 Fix: 티켓 생성 시 조회 누락 해결

--- src/app/user/createticket/page.tsx
+++ src/app/user/createticket/page.tsx
@@ -1,39 +1,43 @@
-"use client";
+'use client';
 
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
-import FirstTaskDrop from "@/components/Tickets/firstTaskDrop";
-import SecondTaskDrop from "@/components/Tickets/secondTaskDrop";
-import Help from "@/components/Modals/Help";
-import ModalHelp from "@/components/Modals/ModalHelp";
-import Modal from "@/components/Modals/Modal";
-import Template from "@/components/Tickets/Template";
-import Button from "@/components/Buttons/Button";
-import { fetchCategories, fetchGuide, postTicket } from "@/services/user";
-import { fetchTemplate } from "@/services/admin";
-import AlertModal from "@/components/Modals/AlertModal";
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import FirstTaskDrop from '@/components/Tickets/firstTaskDrop';
+import SecondTaskDrop from '@/components/Tickets/secondTaskDrop';
+import Help from '@/components/Modals/Help';
+import ModalHelp from '@/components/Modals/ModalHelp';
+import Modal from '@/components/Modals/Modal';
+import Template from '@/components/Tickets/Template';
+import Button from '@/components/Buttons/Button';
+import { fetchCategories, fetchGuide, postTicket } from '@/services/user';
+import { fetchTemplate } from '@/services/admin';
+import AlertModal from '@/components/Modals/AlertModal';
+import { useQueryClient } from '@tanstack/react-query';
 
 export default function UserCreateTicketPage() {
-  const [selectedService, setSelectedService] = useState("1차 카테고리를 선택해주세요.");
-  const [selectedRequestType, setSelectedRequestType] = useState("2차 카테고리를 선택해주세요.");
+  const [selectedService, setSelectedService] =
+    useState('1차 카테고리를 선택해주세요.');
+  const [selectedRequestType, setSelectedRequestType] =
+    useState('2차 카테고리를 선택해주세요.');
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [helpTitle, setHelpTitle] = useState("");
-  const [title, setTitle] = useState("");
-  const [content, setContent] = useState("");
+  const [helpTitle, setHelpTitle] = useState('');
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
   const [isTicketCreated, setIsTicketCreated] = useState(false); // 티켓 생성 여부
   const [categories, setCategories] = useState<any[]>([]);
   const [firstCategories, setFirstCategories] = useState<string[]>([]);
   const [secondCategories, setSecondCategories] = useState<any>();
-  const [helpContent, setHelpContent] = useState("");
+  const [helpContent, setHelpContent] = useState('');
   const [helpFiles, setHelpFiles] = useState([]);
   const [countdown, setCountdown] = useState(1);
   const router = useRouter();
   const [modalState, setModalState] = useState({
     isOpen: false,
-    title: "",
+    title: '',
     btnText: '',
     onClose: () => {},
   });
+  const queryClient = useQueryClient();
 
   const showModal = (title: string, btnText = '닫기') => {
     setModalState({
@@ -41,16 +45,18 @@ export default function UserCreateTicketPage() {
       title,
       btnText,
       onClose: () => {
-        setModalState(prev => ({ ...prev, isOpen: false }));
+        setModalState((prev) => ({ ...prev, isOpen: false }));
       },
     });
   };
 
   const handleServiceChange = (value: string) => {
     setSelectedService(value);
-    const selectedCategory = categories.find((category) => category.name === value);
+    const selectedCategory = categories.find(
+      (category) => category.name === value,
+    );
     setSecondCategories(selectedCategory);
-    
+
     // 첫 번째 카테고리가 변경되면 템플릿을 불러옴
     if (selectedCategory) {
       getTemplate(selectedCategory.categoryId);
@@ -70,10 +76,10 @@ export default function UserCreateTicketPage() {
     const loadCategories = async () => {
       try {
         const response = await fetchCategories();
-        console.log("📌 가져온 카테고리 데이터:", response); // 응답 데이터 확인
+        console.log('📌 가져온 카테고리 데이터:', response); // 응답 데이터 확인
 
-        if (!response || typeof response !== "object") {
-          console.error("⚠️ 잘못된 응답 형식:", response);
+        if (!response || typeof response !== 'object') {
+          console.error('⚠️ 잘못된 응답 형식:', response);
           setCategories([]); // 빈 배열로 초기화
           return;
         }
@@ -86,9 +92,11 @@ export default function UserCreateTicketPage() {
 
         // 정상적인 경우에만 데이터 설정
         setCategories(response.result.categories);
-        setFirstCategories(response.result.categories.map((category: any) => category.name));
+        setFirstCategories(
+          response.result.categories.map((category: any) => category.name),
+        );
       } catch (error) {
-        console.error("❌ 카테고리 조회 실패:", error);
+        console.error('❌ 카테고리 조회 실패:', error);
         setCategories([]); // 에러 발생 시 빈 배열 설정
       }
     };
@@ -100,73 +108,81 @@ export default function UserCreateTicketPage() {
     try {
       const ticketData = {
         title: title,
-        content: content || "",
+        content: content || '',
         categoryId: secondCategories?.childCategories.find(
-          (category: any) => category.name === selectedRequestType
+          (category: any) => category.name === selectedRequestType,
         )?.categoryId,
       };
-  
-      console.log("📌 요청 데이터:", ticketData);
+
+      console.log('📌 요청 데이터:', ticketData);
       const { result, error } = await postTicket(ticketData);
-  
-      if (error) {  // error가 있을 경우 처리
-        console.error("⚠️ 티켓 생성 실패:", error);
-        showModal(error?.response?.data?.result?.title || error?.response?.data?.result?.content);
+
+      if (error) {
+        // error가 있을 경우 처리
+        console.error('⚠️ 티켓 생성 실패:', error);
+        showModal(
+          error?.response?.data?.result?.title ||
+            error?.response?.data?.result?.content,
+        );
         return;
       }
-  
+
       if (!result) {
-        console.error("⚠️ 티켓 생성 실패: 응답 데이터 없음");
-        showModal("티켓 생성에 실패했습니다.");
+        console.error('⚠️ 티켓 생성 실패: 응답 데이터 없음');
+        showModal('티켓 생성에 실패했습니다.');
         return;
       }
-  
-      console.log("✅ 티켓 생성 성공:", result);
+      await queryClient.invalidateQueries({ queryKey: ['user_tickets'] });
+
+      console.log('✅ 티켓 생성 성공:', result);
       setIsTicketCreated(true); // 생성 완료 상태로 변경
       setCountdown(1); // 카운트다운 시작
-      showModal("티켓 생성이 완료되었습니다.");
-      
+      showModal('티켓 생성이 완료되었습니다.');
+
       const timer = setInterval(() => {
         setCountdown((prev) => (prev !== null ? prev - 1 : null));
       }, 1000);
-  
+
       setTimeout(() => {
         clearInterval(timer);
         router.push(`/user/myticket/${result.result.ticketId}`);
       }, 1000);
     } catch (error: any) {
-      console.error("❌ 티켓 생성 중 오류 발생:", error);
-      console.error("📌 오류 상세 정보:", error.response?.data || error.message);
+      console.error('❌ 티켓 생성 중 오류 발생:', error);
+      console.error(
+        '📌 오류 상세 정보:',
+        error.response?.data || error.message,
+      );
       showModal(
         error.response?.data?.message ||
-        error.message ||
-        "티켓 생성 중 문제가 발생했습니다."
+          error.message ||
+          '티켓 생성 중 문제가 발생했습니다.',
       );
     }
   };
 
   const updateHelpContent = async (service: string) => {
-    if (service === "1차 카테고리를 선택해주세요.") return;
+    if (service === '1차 카테고리를 선택해주세요.') return;
 
     try {
       const response = await fetchGuide(secondCategories?.categoryId);
-      console.log("📌 가져온 도움말 데이터:", response);
+      console.log('📌 가져온 도움말 데이터:', response);
 
       if (!response || !response.result || !response.result.content) {
-        console.warn("⚠️ 도움말 내용이 없습니다.");
+        console.warn('⚠️ 도움말 내용이 없습니다.');
         setHelpTitle(`${service}`);
-        setHelpContent("해당 카테고리의 도움말이 없습니다.");
+        setHelpContent('해당 카테고리의 도움말이 없습니다.');
       } else {
         setHelpTitle(`${service} 도움말`);
         setHelpContent(response.result.content);
-        setHelpFiles(response.result.attachmentUrls)
+        setHelpFiles(response.result.attachmentUrls);
       }
 
       setIsModalOpen(true);
     } catch (error) {
-      console.error("❌ 도움말 조회 실패:", error);
+      console.error('❌ 도움말 조회 실패:', error);
       setHelpTitle(`${service} 도움말`);
-      setHelpContent("도움말을 불러오는 중 오류가 발생했습니다.");
+      setHelpContent('도움말을 불러오는 중 오류가 발생했습니다.');
       setIsModalOpen(true);
     }
   };
@@ -174,24 +190,25 @@ export default function UserCreateTicketPage() {
   const getTemplate = async (categoryId: string) => {
     try {
       const response = await fetchTemplate(categoryId);
-      console.log("📌 가져온 템플릿 데이터:", response);
+      console.log('📌 가져온 템플릿 데이터:', response);
 
       if (!response.result.content) {
-        console.warn("⚠️ 템플릿 내용이 없습니다.");
-        setContent("템플릿 내용이 없습니다.");
+        console.warn('⚠️ 템플릿 내용이 없습니다.');
+        setContent('템플릿 내용이 없습니다.');
       } else {
         setContent(response.result.content);
       }
     } catch (error) {
-      console.error("❌ 템플릿 조회 실패:", error);
+      console.error('❌ 템플릿 조회 실패:', error);
     }
   };
 
-  const isHelpButtonVisible = selectedService !== "1차 카테고리를 선택해주세요.";
+  const isHelpButtonVisible =
+    selectedService !== '1차 카테고리를 선택해주세요.';
 
   const isReadyToShowTemp =
-    selectedService !== "1차 카테고리를 선택해주세요." &&
-    selectedRequestType !== "2차 카테고리를 선택해주세요."
+    selectedService !== '1차 카테고리를 선택해주세요.' &&
+    selectedRequestType !== '2차 카테고리를 선택해주세요.';
 
   return (
     <div className="pt-4 pl-6 pr-6 pb-4 flex flex-col justify-between">
@@ -201,13 +218,17 @@ export default function UserCreateTicketPage() {
           <div className="flex flex-col items-start w-80">
             <div className="flex items-start space-x-48">
               <label>업무 분류</label>
-              {selectedService !== "1차 카테고리를 선택해주세요." && (
+              {selectedService !== '1차 카테고리를 선택해주세요.' && (
                 <button
                   className="flex items-center justify-center text-[#6E61CA] hover:text-[#5A50A8] mt-0.5 ml-2"
                   onClick={() => updateHelpContent(selectedService)}
                 >
                   <span className="text-sm font-medium ml-3 mr-1">도움말</span>
-                  <img src="/helpIcon.png" alt="Help Icon" className="w-4 h-4" />
+                  <img
+                    src="/helpIcon.png"
+                    alt="Help Icon"
+                    className="w-4 h-4"
+                  />
                 </button>
               )}
             </div>
@@ -218,7 +239,7 @@ export default function UserCreateTicketPage() {
             />
           </div>
 
-          {selectedService !== "1차 카테고리를 선택해주세요." && (
+          {selectedService !== '1차 카테고리를 선택해주세요.' && (
             <div className="flex flex-col items-start w-80">
               <label>업무</label>
               <SecondTaskDrop
@@ -233,15 +254,19 @@ export default function UserCreateTicketPage() {
       </div>
 
       {isReadyToShowTemp && (
-          <Template title={title} content={content} setTitle={setTitle} setContent={setContent} />
-          
+        <Template
+          title={title}
+          content={content}
+          setTitle={setTitle}
+          setContent={setContent}
+        />
       )}
 
       {isReadyToShowTemp && (
-            <div className="flex justify-center">
-            <Button label="티켓 요청" onClick={handleCreate} color={1} />
-          </div>
-          )}
+        <div className="flex justify-center">
+          <Button label="티켓 요청" onClick={handleCreate} color={1} />
+        </div>
+      )}
 
       {isModalOpen && (
         <ModalHelp onClose={toggleModal}>
@@ -251,10 +276,7 @@ export default function UserCreateTicketPage() {
 
       {modalState.isOpen && (
         <Modal onClose={modalState.onClose}>
-          <AlertModal
-            title={modalState.title}
-            onClick={modalState.onClose}
-          />
+          <AlertModal title={modalState.title} onClick={modalState.onClose} />
         </Modal>
       )}
     </div>
